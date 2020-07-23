f46a0710889048c3f6dad563290a4bb1cc084fef
김예원 <yewon@gim-yewon-ui-MacBookPro.local>
2020-07-23 23:21:22 +0900

    Set react router, helmet, dotenv

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from 'react';
+import { Route } from 'react-router-dom';
+import Main from './main';
+
+class Index extends Component {
+  render() {
+    const pages = [
+      {
+        path: '/',
+        component: Main,
+      },
+    ]
+    return (
+      <Fragment>
+        { pages.map((elem, idx) => <Route key={idx} { ...elem } />) }
+      </Fragment>
+    );
+  }
+}
+
+export default Index;
---
+++ src/pages/main.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+class Main extends Component {
+	render() {
+		return (
+			<Fragment>
+				Hi
+			</Fragment>
+		)
+	}
+}
+
+export default Main;
