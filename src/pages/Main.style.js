6fc1e5f51dd01351fc70ab7742d55519c76423f2
김예원 <yewon@gim-yewon-ui-MacBookPro.local>
2020-07-27 23:12:52 +0900

    Add Header, library-wrapper, map-wrapper className

--- src/pages/Main.style.js
+++ src/pages/Main.style.js
@@ -4,5 +4,15 @@ export const Container = styled.div`
     margin: 0;
     padding: 0;
     display: flex;
+    flex-direction: column;
     justify-content: space-between;
+`;
+
+export const Libraries = styled.div`
+    width: 100%;
+    background-color: #ccffcc;
+    -webkit-border-radius: 6px;
+    border-radius: 6px;
+	margin: 10px auto;
+	border: 1px solid #d9d9d9;
 `;
