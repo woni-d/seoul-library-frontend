370d6b88caf7d6a87e59ed767846f1dc62f0fcc2
Kim Yewon <kyw017763@gmail.com>
2020-07-27 22:49:39 +0900

    Add Skeleton to CustomCard

--- src/components/CustomCard/index.jsx
+++ src/components/CustomCard/index.jsx
@@ -1,6 +1,4 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
 import {
   Card,
   CardActions,
@@ -8,6 +6,8 @@ import {
   Button,
   Typography
 } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
+import './CustomCard.scss';
 
 class CardView extends Component {
   constructor(props) {
@@ -15,48 +15,35 @@ class CardView extends Component {
   }
 
   render() {
-    const { value } = this.props;
+    const { cardItem, loading } = this.props;
     return ( 
       <>
-        <Card variant = "outlined">
+        <Card className='root'>
           <CardContent>
-            <Typography
-              variant = "h6"
-              component = "h6"
-            >
-              No. {value.LBRRY_SEQ_NO} {value.LBRRY_NAME}
+            <Typography variant="h6">
+                {loading ? <Skeleton variant="text" /> : `No. ${cardItem.LBRRY_SEQ_NO} ${cardItem.LBRRY_NAME}`}
             </Typography>
 
             <br />
 
-            <Typography
-              variant = "body1"
-              component = "p"
-            >
-             구명: {value.CODE_VALUE}
+            <Typography variant="body1">
+                {loading ? <Skeleton variant="text" /> : `구명: ${cardItem.CODE_VALUE}`}
             </Typography>
 
-            <Typography
-              variant = "body1"
-              component = "p"
-            >
-              주소: {value.ADRES}
+            <Typography variant="body1">
+                {loading ? <Skeleton variant="text" /> : `주소: ${cardItem.ADRES}`}
             </Typography>
 
-            <Typography
-              variant = "body1"
-              component = "p"
-            >
-              정기 휴관일: {value.FDRM_CLOSE_DATE}
+            <Typography variant="body1">
+                {loading ? <Skeleton variant="text" /> : `정기 휴관일: ${cardItem.FDRM_CLOSE_DATE}`}
             </Typography>
 
-            <Typography
-              variant = "body1"
-              component = "p"
-            >
-              전화번호: {value.TEL_NO}
+            <Typography variant="body1">
+                {loading ? <Skeleton variant="text" /> : `전화번호: ${cardItem.TEL_NO}`}
             </Typography>
-            
+
+            <Skeleton variant="rect" animation="wave" height={250} />
+
           </CardContent>
           <CardActions>
             <Button>Map</Button>
