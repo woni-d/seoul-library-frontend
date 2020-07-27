6b55c0ef498d10b8508ed1e6f41198f44448ac90
김예원 <yewon@gim-yewon-ui-MacBookPro.local>
2020-07-28 00:46:16 +0900

    Add LinearProgress, CustomPagination

---
+++ src/components/CustomPagination/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Pagination } from '@material-ui/lab';
+import { makeStyles } from '@material-ui/core/styles';
+
+const CustomPagination = (props) => {
+  const { totalPage, currentPage, handlePagination } = props;
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        marginTop: theme.spacing(2),
+      },
+    },
+  }));
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Pagination
+        count={totalPage}
+        page={currentPage}
+        onChange={handlePagination}
+      />
+    </div>
+  )
+}
+
+export default CustomPagination;
