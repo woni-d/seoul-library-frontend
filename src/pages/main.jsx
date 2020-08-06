ebc68a810e629630be147c2bb843677363ac12e7
Kim Yewon <kyw017763@gmail.com>
2020-08-06 23:56:00 +0900

    Fix to get count by district

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -8,6 +8,8 @@ import LinearProgress from '../components/LinearProgress';
 import CustomPagination from '../components/CustomPagination';
 import { Container, Libraries } from './Main.style';
 
+const apiKey = process.env.REACT_APP_SEOUL_API_KEY;
+
 class Main extends Component {
   constructor(props) {
     super(props);
@@ -15,11 +17,113 @@ class Main extends Component {
       libraryAllCount: null,
       libraryList: null,
       libraryListLength: null,
-      libraryTotalCount: (53 - 0) + 1,
-      libraryStartCount: 0,
-      libraryEndCount: 53,
+      libraryTotalCount: 1,
+      libraryStartCount: 1, // Open API 인덱스 1부터 시작
+      libraryEndCount: 1,
+      districtOptionKeys: [],
+      districtOption: null,
+        // '강남구': {
+        //   start: 1,
+        //   end: 53,
+        // },
+        // '강동구': {
+        //   start: 54,
+        //   end: 104,
+        // },
+        // '강북구': {
+        //   start: 105,
+        //   end: 157,
+        // },
+        // '강서구': {
+        //   start: 158,
+        //   end: 239,
+        // },
+        // '관악구': {
+        //   start: 240,
+        //   end: 293,
+        // },
+        // '광진구': {
+        //   start: 294,
+        //   end: 330,
+        // },
+        // '구로구': {
+        //   start: 331,
+        //   end: 411,
+        // },
+        // '금천구': {
+        //   start: 412,
+        //   end: 436,
+        // },
+        // '노원구': {
+        //   start: 437,
+        //   end: 482,
+        // },
+        // '도봉구': {
+        //   start: 483,
+        //   end: 526,
+        // },
+        // '동대문구': {
+        //   start: 527,
+        //   end: 568,
+        // },
+        // '동작구': {
+        //   start: 569,
+        //   end: 620,
+        // },
+        // '마포구': {
+        //   start: 621,
+        //   end: 674,
+        // },
+        // '서대문구': {
+        //   start: 675,
+        //   end: 709,
+        // },
+        // '성동구': {
+        //   start: 779,
+        //   end: 812,
+        // },
+        // '성북구': {
+        //   start: 813,
+        //   end: 880,
+        // },
+        // '서초구': {
+        //   start: 710,
+        //   end: 778,
+        // },
+        // '송파구': {
+        //   start: 881,
+        //   end: 962,
+        // },
+        // '영등포구': {
+        //   start: 1013,
+        //   end: 1068,
+        // },
+        // '용산구': {
+        //   start: 1069,
+        //   end: 1124,
+        // },
+        // '양천구': {
+        //   start: 963,
+        //   end: 1012,
+        // },
+        // '은평구': {
+        //   start: 1125,
+        //   end: 1222,
+        // },
+        // '종로구': {
+        //   start: 1223,
+        //   end: 1272,
+        // },
+        // '중구': {
+        //   start: 1273,
+        //   end: 1315,
+        // },
+        // '중랑구': {
+        //   start: 1316,
+        //   end: 1366,
+        // },
       searchOption: 'selectedDistrict',
-      selectedDistrict: '강남구',
+      selectedDistrict: '-----',
       searchText: '',
       libraryCountPerPage: 5, // default
       currentPage: 1,
@@ -30,17 +134,50 @@ class Main extends Component {
   }
 
   async componentDidMount() {
-    // TODO: 구별 카운트 정의
     await this.getLibraryInfo();
+
+    const { libraryAllCount } = this.state;
+    let libraryList = []
+    for (let i = 0; i < Math.floor(libraryAllCount / 1000) + (libraryAllCount % 1000 ? 1 : 0); i++) {
+      const libraryApiUri = `http://openapi.seoul.go.kr:8088/${apiKey}/json/SeoulLibraryTimeInfo/${(i * 1000)}/${((i + 1) * 999)}`;
+      const { data: { SeoulLibraryTimeInfo: { row } } } = await axios.get(libraryApiUri);
+      libraryList = [...libraryList, ...row];
+    }
+
+    const districtOption = {}
+
+    libraryList.forEach((elem, idx) => { // idx는 0부터 시작
+      if (districtOption.hasOwnProperty(elem['CODE_VALUE'])) {
+        districtOption[elem['CODE_VALUE']].end ++;
+      }
+      else {
+        districtOption[elem['CODE_VALUE']] = {
+          start: idx + 1,
+          end: idx + 1,
+        }
+      }
+    })
+
+    const districtOptionKeys = Object.keys(districtOption)
+    const firstDistrictOption = districtOptionKeys[0]
+
+    this.setState({
+      districtOptionKeys,
+      districtOption,
+      selectedDistrict: firstDistrictOption,
+      libraryTotalCount: districtOption[firstDistrictOption].end - districtOption[firstDistrictOption].start,
+      libraryStartCount: districtOption[firstDistrictOption].start,
+      libraryEndCount: districtOption[firstDistrictOption].start + 5,
+
+    }, this.getLibraryInfo)
   }
 
   async getLibraryInfo() {
-    const { searchOption, libraryStartCount, currentPage, libraryCountPerPage } = this.state;
-    const apiKey = process.env.REACT_APP_SEOUL_API_KEY;
+    const { searchOption, libraryStartCount, libraryEndCount, currentPage, libraryCountPerPage } = this.state;
 
     if (searchOption === 'selectedDistrict') {
-      const diff = (currentPage * libraryCountPerPage)
-      const libraryApiUri = `http://openapi.seoul.go.kr:8088/${apiKey}/json/SeoulLibraryTimeInfo/${libraryStartCount + diff}/${libraryStartCount + diff + (libraryCountPerPage - 1)}`;
+      const diff = ((currentPage - 1) * libraryCountPerPage)
+      const libraryApiUri = `http://openapi.seoul.go.kr:8088/${apiKey}/json/SeoulLibraryTimeInfo/${libraryStartCount + diff}/${libraryEndCount + diff}`;
       try {
         const { config, data: { SeoulLibraryTimeInfo: { list_total_count, row } }, headers, request, status, statusText } = await axios.get(libraryApiUri);
         this.setState({
@@ -60,12 +197,12 @@ class Main extends Component {
     else {
       const { searchText, libraryAllCount } = this.state;
       try {
-        // TODO: handleChange 말고 검색
         // TODO: KeyPress 이벤트
         let libraryList = []
         for (let i = 0; i < Math.floor(libraryAllCount / 1000) + (libraryAllCount % 1000 ? 1 : 0); i++) {
           const libraryApiUri = `http://openapi.seoul.go.kr:8088/${apiKey}/json/SeoulLibraryTimeInfo/${(i * 1000)}/${((i + 1) * 999)}`;
           const { config, data: { SeoulLibraryTimeInfo: { list_total_count, row } }, headers, request, status, statusText } = await axios.get(libraryApiUri);
+          // TODO: statusText나 status로 
           libraryList = [...libraryList, ...row];
         }
 
@@ -99,7 +236,7 @@ class Main extends Component {
 
   handleSearch = (e) => this.setState({ currentPage: 0 }, this.getLibraryInfo)
 
-  handlePagination = (e, value) => this.setState({ currentPage: value}, this.getLibraryInfo)
+  handlePagination = (e, value) => this.setState({ currentPage: value }, this.getLibraryInfo)
 
   handleSearchOptionChange = (value) => e => {
     this.setState({
@@ -111,109 +248,10 @@ class Main extends Component {
     const stateObj = {};
     if (e.target.name === 'selectedDistrict') {
       let selectedDistrict = e.target.value;
+      const { districtOption } = this.state;
       let start, end;
-      switch (selectedDistrict) {
-        case '강동구':
-          start = 54;
-          end = 104;
-          break;
-        case '강북구':
-          start = 105;
-          end = 157;
-          break;
-        case '강서구':
-          start = 158;
-          end = 239;
-          break;
-        case '관악구':
-          start = 240;
-          end = 293;
-          break;
-        case '광진구':
-          start = 294;
-          end = 330;
-          break;
-        case '구로구':
-          start = 331;
-          end = 411;
-          break;
-        case '금천구':
-          start = 412;
-          end = 436;
-          break;
-        case '노원구':
-          start = 437;
-          end = 482;
-          break;
-        case '도봉구':
-          start = 483;
-          end = 526;
-          break;
-        case '동대문구':
-          start = 527;
-          end = 568;
-          break;
-        case '동작구':
-          start = 569;
-          end = 620;
-          break;
-        case '마포구':
-          start = 621;
-          end = 674;
-          break;
-        case '서대문구':
-          start = 675;
-          end = 709;
-          break;
-        case '성동구':
-          start = 779;
-          end = 812;
-          break;
-        case '성북구':
-          start = 813;
-          end = 880;
-          break;
-        case '서초구':
-          start = 710;
-          end = 778;
-          break;
-        case '송파구':
-          start = 881;
-          end = 962;
-          break;
-        case '영등포구':
-          start = 1013;
-          end = 1068;
-          break;
-        case '용산구':
-          start = 1069;
-          end = 1124;
-          break;
-        case '양천구':
-          start = 963;
-          end = 1012;
-          break;
-        case '은평구':
-          start = 1125;
-          end = 1222;
-          break;
-        case '종로구':
-          start = 1223;
-          end = 1272;
-          break;
-        case '중구':
-          start = 1273;
-          end = 1315;
-          break;
-        case '중랑구':
-          start = 1316;
-          end = 1366;
-          break;
-        default:
-          selectedDistrict = '강남구';
-          start = 1;
-          end = 53;
-      }
+      start = districtOption[selectedDistrict].start;
+      end = districtOption[selectedDistrict].end;
       stateObj['libraryStartCount'] = start;
       stateObj['libraryEndCount'] = end;
       stateObj['libraryTotalCount'] = (end - start);
@@ -229,13 +267,14 @@ class Main extends Component {
       [e.target.name]: e.target.value,
       ...stateObj,
       currentPage: 1,
-    }, this.getLibraryInfo);
+    } , this.getLibraryInfo );
   }
   
 	render() {
     const {
       successAlertOpen,
       errorAlertOpen,
+      districtOptionKeys,
       libraryList,
       libraryListLength,
       libraryTotalCount,
@@ -250,34 +289,6 @@ class Main extends Component {
       libraryCountPerPage,
       currentPage
     } = this.state;
-
-    const districtList = [
-      '강남구',
-      '강동구',
-      '강서구',
-      '강북구',
-      '관악구',
-      '광진구',
-      '구로구',
-      '금천구',
-      '노원구',
-      '동대문구',
-      '도봉구',
-      '동작구',
-      '마포구',
-      '서대문구',
-      '성동구',
-      '성북구',
-      '서초구',
-      '송파구',
-      '영등포구',
-      '용산구',
-      '양천구',
-      '은평구',
-      '종루구',
-      '중구',
-      '중랑구',
-    ];
 
     const searchOptionObj = {
       district: {
@@ -301,11 +312,13 @@ class Main extends Component {
     if (!libraryList) {
       return null;
     }
+
     if (libraryListLength > 0) {
       let maxPageCount = Math.floor((libraryListLength - 1) / libraryCountPerPage)
       if (maxPageCount < 0) maxPageCount = 0
       if (currentPage > maxPageCount) currentPage = maxPageCount
     }
+
     function Alert(props) {
       return <MuiAlert elevation={6} variant="filled" {...props} />;
     }
@@ -313,7 +326,7 @@ class Main extends Component {
     return (
       <Container>
         <Header
-          districtList={districtList}
+          districtList={districtOptionKeys}
           searchOption={searchOption}
           searchOptionObj={searchOptionObj}
           searchTextOptionArr={searchTextOptionArr}
@@ -352,6 +365,7 @@ class Main extends Component {
               />
             ))
           }
+          {/* { TODO: 페이지네이션 조정 } */}
           <CustomPagination totalPage={Math.floor((libraryTotalCount / libraryCountPerPage) + 1)} currentPage={currentPage} handlePagination={this.handlePagination} />
         </Libraries>
 
